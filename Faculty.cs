0a3b9d7e2006f39803c1a9917644e9092ba71d9c
CS24786 <nawaztalha078@gmail.com>
2025-03-21 21:21:16 +0500

    Database project

---
+++ Faculty.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace facultysystem
+{
+    public partial class Faculty : Form
+    {
+        public Faculty()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "Faculty";
+            dataGridView1.Columns[1].Name = "Course";
+            dataGridView1.Columns[2].Name = "Semester";
+            dataGridView1.Columns[3].Name = "Hours";
+
+            // Add rows (example data)
+            dataGridView1.Rows.Add("Dr. Smith", "C++ Programming", "Spring 2025", "3");
+            dataGridView1.Rows.Add("Prof. John", "AI Fundamentals", "Fall 2024", "4");
+        }
+    }
+}
---
+++ MainForm.Designer.cs
@@ -0,0 +1,443 @@
+﻿namespace facultysystem
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel2 = new Panel();
+            label4 = new Label();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            panel3 = new Panel();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            label6 = new Label();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            label5 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Indigo;
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(button6);
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(251, 557);
+            panel2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(58, 529);
+            label4.Name = "label4";
+            label4.Size = new Size(76, 21);
+            label4.TabIndex = 2;
+            label4.Text = "Sign Out";
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Indigo;
+            button6.Cursor = Cursors.Hand;
+            button6.FlatStyle = FlatStyle.Popup;
+            button6.ForeColor = SystemColors.ButtonHighlight;
+            button6.Image = Properties.Resources.icons8_sign_out_30;
+            button6.Location = new Point(11, 507);
+            button6.Name = "button6";
+            button6.Size = new Size(41, 43);
+            button6.TabIndex = 2;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.Cursor = Cursors.Hand;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Image = Properties.Resources.icons8_request_24;
+            button5.ImageAlign = ContentAlignment.TopLeft;
+            button5.Location = new Point(24, 441);
+            button5.Name = "button5";
+            button5.Size = new Size(199, 32);
+            button5.TabIndex = 2;
+            button5.Text = "     Requests";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Cursor = Cursors.Hand;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Image = Properties.Resources.icons8_course_assign_24;
+            button4.ImageAlign = ContentAlignment.TopLeft;
+            button4.Location = new Point(24, 388);
+            button4.Name = "button4";
+            button4.Size = new Size(199, 36);
+            button4.TabIndex = 2;
+            button4.Text = "      Assignments";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Indigo;
+            button3.Cursor = Cursors.Hand;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Image = Properties.Resources.icons8_e_learning_24;
+            button3.ImageAlign = ContentAlignment.TopLeft;
+            button3.Location = new Point(24, 336);
+            button3.Name = "button3";
+            button3.Size = new Size(199, 35);
+            button3.TabIndex = 2;
+            button3.Text = "     Courses";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Indigo;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Image = Properties.Resources.icons8_people_24;
+            button2.ImageAlign = ContentAlignment.TopLeft;
+            button2.Location = new Point(24, 286);
+            button2.Name = "button2";
+            button2.Size = new Size(199, 32);
+            button2.TabIndex = 2;
+            button2.Text = "         Faculty Manage";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Indigo;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Image = Properties.Resources.icons8_home_24;
+            button1.ImageAlign = ContentAlignment.TopLeft;
+            button1.Location = new Point(24, 236);
+            button1.Name = "button1";
+            button1.Size = new Size(199, 34);
+            button1.TabIndex = 2;
+            button1.Text = "Home";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.Control;
+            label3.Location = new Point(53, 204);
+            label3.Name = "label3";
+            label3.Size = new Size(137, 21);
+            label3.TabIndex = 2;
+            label3.Text = "WELCOME, USER";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Image = Properties.Resources.Screenshot__337__1;
+            pictureBox1.Location = new Point(53, 14);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(136, 177);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.BorderStyle = BorderStyle.FixedSingle;
+            panel3.Controls.Add(dataGridView1);
+            panel3.Controls.Add(label6);
+            panel3.Controls.Add(textBox4);
+            panel3.Controls.Add(textBox3);
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(textBox1);
+            panel3.Controls.Add(button7);
+            panel3.Controls.Add(button8);
+            panel3.Controls.Add(button9);
+            panel3.Controls.Add(label5);
+            panel3.Controls.Add(label1);
+            panel3.Controls.Add(label2);
+            panel3.Controls.Add(label7);
+            panel3.Controls.Add(label8);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(251, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(669, 557);
+            panel3.TabIndex = 2;
+            panel3.Paint += panel3_Paint;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4 });
+            dataGridView1.Location = new Point(97, 372);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(465, 136);
+            dataGridView1.TabIndex = 55;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Faculty Name";
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Resource";
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Quantity";
+            Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Status";
+            Column4.Name = "Column4";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(265, 330);
+            label6.Name = "label6";
+            label6.Size = new Size(126, 25);
+            label6.TabIndex = 54;
+            label6.Text = "Requests List";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(379, 218);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(225, 23);
+            textBox4.TabIndex = 53;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(379, 136);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(225, 23);
+            textBox3.TabIndex = 52;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(63, 218);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(227, 23);
+            textBox2.TabIndex = 51;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(63, 136);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(227, 23);
+            textBox1.TabIndex = 50;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Indigo;
+            button7.Cursor = Cursors.Hand;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.ForeColor = SystemColors.Control;
+            button7.Location = new Point(459, 274);
+            button7.Name = "button7";
+            button7.Size = new Size(103, 34);
+            button7.TabIndex = 49;
+            button7.Text = "Reject";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Indigo;
+            button8.Cursor = Cursors.Hand;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.ForeColor = SystemColors.Control;
+            button8.Location = new Point(283, 274);
+            button8.Name = "button8";
+            button8.Size = new Size(104, 34);
+            button8.TabIndex = 48;
+            button8.Text = "Approve";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Indigo;
+            button9.Cursor = Cursors.Hand;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.ForeColor = Color.White;
+            button9.Location = new Point(97, 274);
+            button9.Name = "button9";
+            button9.Size = new Size(106, 34);
+            button9.TabIndex = 47;
+            button9.Text = "Submit";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(379, 177);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 21);
+            label5.TabIndex = 46;
+            label5.Text = "Status";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(63, 177);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 21);
+            label1.TabIndex = 45;
+            label1.Text = "Quantity";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(379, 103);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 21);
+            label2.TabIndex = 44;
+            label2.Text = "Resource Type";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(63, 103);
+            label7.Name = "label7";
+            label7.Size = new Size(115, 21);
+            label7.TabIndex = 43;
+            label7.Text = "Faculty Name";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(220, 47);
+            label8.Name = "label8";
+            label8.Size = new Size(194, 30);
+            label8.TabIndex = 42;
+            label8.Text = "Resource Requests";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(920, 557);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "MainForm";
+            Text = "MainForm";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel2;
+        private Label label3;
+        private PictureBox pictureBox1;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button button6;
+        private Label label4;
+        private Panel panel3;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private Label label6;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label label5;
+        private Label label1;
+        private Label label2;
+        private Label label7;
+        private Label label8;
+    }
+}
